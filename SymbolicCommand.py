3e1a2fbd4a0a25344613f0ba189eee6e12b1e13b
Luo Qisheng <luoqisheng@bytedance.com>
2018-12-15 15:19:27 +0800

    init

---
+++ SymbolicCommand.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import lldb
+import fblldbbase as fb
+
+def lldbcommands():
+  return [ SymbolicAddresses() ]
+  
+class SymbolicAddresses(fb.FBCommand):
+  def name(self):
+    return 'symbolic'
+    
+  def description(self):
+    return 'symbolic addresses'
+    
+  def run(self, arguments, options):
+    # It's a good habit to explicitly cast the type of all return
+    # values and arguments. LLDB can't always find them on its own.
+    interpreter = lldb.debugger.GetCommandInterpreter()
+    line = -1
+    for arg in arguments:
+      line = line + 1
+      object = lldb.SBCommandReturnObject()
+      arg = arg.strip()
+      interpreter.HandleCommand('image lookup -a {}'.format(arg), self.context, object)
+      if object.GetOutput():
+        print >> self.result, object.GetOutput().strip().splitlines()[1].replace('Summary:', 'frame #{}: {}'.format(line, arg))
